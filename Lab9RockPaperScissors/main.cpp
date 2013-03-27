c32d561bc0a4536f496c43466074bdc9d661e64c
mwilliammyers <mwilliammyers@gmail.com>
2013-03-26 20:21:01 -0600

    Initial Commit

---
+++ Lab9RockPaperScissors/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab9RockPaperScissors
+//
+//  Created by William Myers on 3/26/13.
+//  Copyright (c) 2013 William Myers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
